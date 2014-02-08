1adcf5fe6a625f4da3a7f31cf2fbc4548f409602
jpardobl <jpardo@digitalhigh.es>
2014-02-08 11:14:21 +0100

    incr version

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-haweb',
-    version = '0.2.1',
-    packages = ['haweb', "haweb.management.commands"],
+    version = '0.2.2',
+    packages = ['haweb', "haweb.management", "haweb.management.commands"],
     include_package_data = True,
     license = 'BSD License',
     description = 'A Django app which shows a web front for django-hautomation',
