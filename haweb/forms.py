05205a5fccf409b6f89798da7dabf8f4ef330e6b
jpardobl <kit2707888>
2014-08-31 22:01:54 +0200

    made button desapear

--- haweb/forms.py
+++ haweb/forms.py
@@ -5,10 +5,7 @@ from hautomation_x10 import settings
 class ControlForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ControlForm, self).__init__(*args, **kwargs)
-        self.fields["protocol"] = forms.ChoiceField(choices=[ (x, x) for x in haclient.get_protocols(
-            settings.HA_SERVER,
-            settings.HA_USERNAME,
-            settings.HA_PASSWORD,)])
+        self.fields["protocol"] = forms.ChoiceField(choices=["GPIO", "X10"])
 
         self.fields["protocol"].widget.attrs = {"class": "span3"}
         self.fields["did"].widget.attrs = {"class": "span4"}
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-haweb',
-    version = '0.2.3',
+    version = '0.2.4',
     packages = ['haweb', "haweb.management", "haweb.management.commands"],
     include_package_data = True,
     license = 'BSD License',
